52dfb845c258f19ebd807b0257ce64a2c614815f
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-08-21 11:28:19 +0545

    Udpated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # clone this website and extract the data 
-import requests,asyncio,telegram
+import requests,asyncio,telegram,datetime
 
 
 bot = telegram.Bot(token='7400655935:AAG7X6LSfYDZZJWQQRU0wLyGoHkwdoaH17k')
@@ -12,22 +12,20 @@ async def send_message(message):
         await bot.send_message(chat_id=channel_id, text=message[i:i + chunk_size])
         print("Message sent")
 
-loop = asyncio.get_event_loop()
 
-url = "https://nepalipatro.com.np/rashifal/getv5/type/dwmy?lang=en"
-response = requests.get(url)
-print("Data fetched")
-data= response.json()["np"][0]
-
-horoscopedata = {}
-horoscope=["aries","taurus","gemini","cancer","leo","virgo","libra","scorpio","sagittarius","capricorn","aquarius","pisces"]
-for i in horoscope:
-    horoscopedata[i] = data[i]
-
-date = data['title'].split("|")[1].split(".")[0]
-
-message=f"Horoscope of {date}\n"
-for i in horoscopedata:
-    message+=f"{i.title()}:\n{horoscopedata[i]}\n\n"
-print("Message created")
-loop.run_until_complete(send_message(message))
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    url = "https://nepalipatro.com.np/rashifal/getv5/type/dwmy?lang=en"
+    response = requests.get(url)
+    print("Data fetched")
+    data= response.json()["np"][0]
+    horoscopedata = {}
+    horoscope=["aries","taurus","gemini","cancer","leo","virgo","libra","scorpio","sagittarius","capricorn","aquarius","pisces"]
+    for i in horoscope:
+        horoscopedata[i] = data[i]
+    date = datetime.datetime.now().strftime("%A %d-%B %Y")
+    message=f"Horoscope of {date}\n"
+    for i in horoscopedata:
+        message+=f"{i.title()}:\n{horoscopedata[i]}\n\n"
+    print("Message created")
+    loop.run_until_complete(send_message(message))
